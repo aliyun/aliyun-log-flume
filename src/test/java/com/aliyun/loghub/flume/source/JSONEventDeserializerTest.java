4e09a668841ff2c5d51d5a2ee946da72fcf3a1ae
wb-cjh663673 <wb-cjh663673@alibaba-inc.com>
2020-02-24 11:33:14 +0800

    unit test

--- src/test/java/com/aliyun/loghub/flume/source/JSONEventDeserializerTest.java
+++ src/test/java/com/aliyun/loghub/flume/source/JSONEventDeserializerTest.java
@@ -2,6 +2,8 @@ package com.aliyun.loghub.flume.source;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import org.apache.flume.Context;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -20,5 +22,14 @@ public class JSONEventDeserializerTest {
         assertTrue(JSONEventDeserializer.mayBeJSON(validJSONObject));
         Object result = JSONEventDeserializer.parseJSONObjectOrArray(validJSONObject);
         assertTrue(result instanceof JSONObject);
+    }
+
+    @Test
+    public void testAddTopic(){
+        Context context = new Context();
+        //context.put("topicAsField","true");
+        JSONEventDeserializer jsonEventDeserializer = new JSONEventDeserializer();
+        jsonEventDeserializer.configure(context);
+        System.out.println(new Gson().toJson(jsonEventDeserializer));
     }
 }
